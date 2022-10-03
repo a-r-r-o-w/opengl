09a9d68cd2c1234ba4df04e336ccb6357e4598bf
a-r-r-o-w <avs050602@gmail.com>
2022-10-03 20:57:11 +0530

    update shaders to support triangle colouring using both vertex data and uniforms

--- interactive-objects/src/shaders/fragment.shader.glsl
+++ interactive-objects/src/shaders/fragment.shader.glsl
@@ -1,9 +1,15 @@
 #version 330 core
 
+in vec3 color;
+
+uniform bool u_use_vertex_color;
+uniform vec3 u_color;
+
 out vec4 FragColor;
 
-uniform vec4 u_color;
-
 void main () {
-  FragColor = u_color;
+  if (u_use_vertex_color)
+    FragColor = vec4(color.xyz, 1.0f);
+  else
+    FragColor = vec4(u_color, 1.0f);
 }
--- interactive-objects/src/shaders/vertex.shader.glsl
+++ interactive-objects/src/shaders/vertex.shader.glsl
@@ -1,12 +1,15 @@
 #version 330 core
 
-layout (location = 0) in vec3 pos;
+layout (location = 0) in vec3 i_pos;
+layout (location = 1) in vec3 i_color;
+
+out vec3 color;
 
 uniform mat4 u_model;
 uniform mat4 u_view;
 uniform mat4 u_projection;
 
 void main () {
-  gl_Position = u_projection * u_view * u_model * vec4(pos.xyz, 1.0);
-  // gl_Position = vec4(pos.xyz, 1.0);
+  gl_Position = u_projection * u_view * u_model * vec4(i_pos.xyz, 1.0);
+  color = i_color;
 }
