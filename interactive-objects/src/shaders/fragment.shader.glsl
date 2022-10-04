114cd772d61894af53dde0dbf3a46ff93f21934b
a-r-r-o-w <avs050602@gmail.com>
2022-10-04 15:26:11 +0530

    update shaders to support blending

--- interactive-objects/src/shaders/fragment.shader.glsl
+++ interactive-objects/src/shaders/fragment.shader.glsl
@@ -3,13 +3,15 @@
 in vec3 color;
 
 uniform bool u_use_vertex_color;
+uniform float u_blend;
 uniform vec3 u_color;
 
 out vec4 FragColor;
 
 void main () {
-  if (u_use_vertex_color)
-    FragColor = vec4(color.xyz, 1.0f);
+  if (u_use_vertex_color) {
+    FragColor = vec4(color.xyz, u_blend);
+  }
   else
     FragColor = vec4(u_color, 1.0f);
 }
